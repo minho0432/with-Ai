e8c733947f1e7c46ca83d9260f6e744fdbf8e990
minho04320916 <minho04320916@gmail.com>
2026-02-28 07:16:45 +0000

    Initial commit: Add portfolio files

--- main.js
+++ main.js
@@ -1,44 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const championGrid = document.querySelector('.champion-grid');
+document.addEventListener('mousemove', e => {
+    const profileCard = document.querySelector('.profile-card');
+    const { clientX: x, clientY: y } = e;
+    const { left, top, width, height } = profileCard.getBoundingClientRect();
 
-    const champions = [
-        {
-            name: 'Garen',
-            image: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Garen_0.jpg'
-        },
-        {
-            name: 'Jinx',
-            image: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jinx_0.jpg'
-        },
-        {
-            name: 'Lux',
-            image: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lux_0.jpg'
-        }
-    ];
+    const rotateX = (y - (top + height / 2)) / 20;
+    const rotateY = -(x - (left + width / 2)) / 20;
 
-    champions.forEach(champion => {
-        const card = document.createElement('div');
-        card.classList.add('champion-card');
-
-        const img = document.createElement('img');
-        img.src = champion.image;
-        img.alt = champion.name;
-
-        const name = document.createElement('h3');
-        name.textContent = champion.name;
-
-        card.appendChild(img);
-        card.appendChild(name);
-        championGrid.appendChild(card);
-    });
-
-    document.querySelectorAll('nav a').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
+    profileCard.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 });
